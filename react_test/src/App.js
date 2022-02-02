b6e1148a5e95463feb65244dc5855cc1de2160a8
sungwookang <sungwookang11@gmail.com>
2022-02-03 01:16:31 +0900

    ...

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import React, { useState } from "react";
+import {useSpring, animated} from "react-spring";
 
 const App = () => {
-  let count = 0;
-  
+  const [flip, setFlip] = useState(false);
+  const ani = useSpring({
+    to: { opacity: 0 },
+    from: { opacity: 1 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    onRest: ()=> setFlip(!flip),
+  });
+
 
   
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <button onClick={
-        () => {}
-      }>
-      up
-      </button>
+      <animated.h1 style={ani}>Hello react</animated.h1>
     </div>
   );
 }
